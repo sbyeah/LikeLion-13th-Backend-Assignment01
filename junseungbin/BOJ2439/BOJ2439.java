d3c1814a6496cf81daebbfcdd5320ba48c7a3685
sbyeah <kskb222@gmail.com>
2025-04-06 17:18:34 +0900

    Update_2 BOJ2439.java

--- junseungbin/BOJ2439/BOJ2439.java
+++ junseungbin/BOJ2439/BOJ2439.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 class BOJ2439 {
     public static void main(String[] args) {
-        int count = 0;
+        int count;
         Scanner sc = new Scanner(System.in);
         int limit = sc.nextInt();
-        while(count < limit){
-            count++;
+        sc.close();
+        for(count = 0;count < limit; count++){
             for(int n = limit ; n > count ; n--){
                 System.out.print(" ");
             }
             for(int m = 0 ; m < count ; m++){
                 System.out.print("*");
             }
-            System.out.print("\n");
+            System.out.println("");
 
         }
     }
