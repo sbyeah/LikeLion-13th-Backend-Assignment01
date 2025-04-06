260ddd988427be5cc14a00b55e4413d9a8359525
sbyeah <kskb222@gmail.com>
2025-04-06 21:31:23 +0900

    Update_pls_final ProgrammersList.java

--- junseungbin/ProgrammersList/ProgrammersList.java
+++ junseungbin/ProgrammersList/ProgrammersList.java
@@ -1,30 +1,27 @@
-package assignment;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
-class ProgrammersList {
+class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
+        int[] answer = {};
+
         int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
-        int[] answer = {};
 
         if (n == 1) {
-            answer = new int[b]; 
-            System.arraycopy(num_list, 0, answer, 0, b); 
+            answer = Arrays.copyOfRange(num_list, 0, b + 1);
         } else if (n == 2) {
-            int length = num_list.length - (a - 1); 
-            answer = new int[length];
-            System.arraycopy(num_list, a - 1, answer, 0, length);
+            answer = Arrays.copyOfRange(num_list, a, num_list.length);
         } else if (n == 3) {
-            int length = b - a+1;
-            answer = new int[length];
-            System.arraycopy(num_list, a-1, answer, 0, length);
+            answer = Arrays.copyOfRange(num_list, a, b + 1);
         } else if (n == 4) {
-            int length = ((b - a) / c + 1);
-            answer = new int[length];
-            for (int i = 0; i < length; i++) {
-                answer[i] = num_list[a + i * c - 1];
+            int size = (b - a) / c + 1;
+            answer = new int[size];
+            
+            int index = 0;
+            for (int i = a; i <= b; i += c) {
+                answer[index++] = num_list[i];
             }
         }
 
@@ -33,37 +30,34 @@ class ProgrammersList {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ProgrammersList solution = new ProgrammersList();
 
-        System.out.print("1.숫자열의 0번 인덱스부터 b번 인덱스까지");
-        System.out.print("\n2.숫자열의 a번 인덱스부터 마지막 인덱스까지");
-        System.out.print("\n3.숫자열의 a번 인덱스부터 b번 인덱스까지");
-        System.out.print("\n4.숫자열의 0번 인덱스부터 b번 인덱스까지 c의 간격으로");
-        System.out.print("\n번호 입력(1, 2, 3, 4): ");
+        System.out.println("1.숫자열의 0번 인덱스부터 b번 인덱스까지");
+        System.out.println("2.숫자열의 a번 인덱스부터 마지막 인덱스까지");
+        System.out.println("3.숫자열의 a번 인덱스부터 b번 인덱스까지");
+        System.out.println("4.숫자열의 0번 인덱스부터 b번 인덱스까지 c의 간격으로");
+        System.out.print("번호 입력(1, 2, 3, 4): ");
         int n = sc.nextInt();
-
-        System.out.print("시작인덱스, 마지막 인덱스, 출력 간격을 순서대로 입력(a, b, c): ");
+        
+        System.out.print("시작 인덱스, 마지막 인덱스, 출력 인덱스간의 간격 순서대로 입력: ");
         int[] slicer = new int[3];
-        for (int i = 0; i < 3; i++) {
-            slicer[i] = sc.nextInt();
-        }
-
-        System.out.print("숫자열의 크기와 요소들을 하나씩 순서대로 입력하세요: ");
-        int size = sc.nextInt();
-        int[] num_list = new int[size];
-        for (int i = 0; i < size; i++) {
+        slicer[0] = sc.nextInt();
+        slicer[1] = sc.nextInt();
+        slicer[2] = sc.nextInt();
+        
+        System.out.print("숫자열의 크기 입력: ");
+        int numListSize = sc.nextInt();
+        
+        int[] num_list = new int[numListSize];
+        System.out.print("숫자열의 숫자 입력: ");
+        for (int i = 0; i < numListSize; i++) {
             num_list[i] = sc.nextInt();
         }
 
-        int[] result = solution.solution(n, slicer, num_list);
-
-        System.out.print("결과: [");
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i]);
-            if (i < result.length - 1) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println("]");
+        Solution sol = new Solution();
+        int[] result = sol.solution(n, slicer, num_list);
+        
+        System.out.println("결과: " + Arrays.toString(result));
+        
+        sc.close();
     }
 }
